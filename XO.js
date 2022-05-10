4d8676f9ae5bfca593e92259e2c85a8ebe9d06bf
Damri <damri@hp.com>
2022-05-10 19:02:39 +0300

    created board

--- XO.js
+++ XO.js
@@ -1,3 +1,16 @@
 let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]]
 const board = document.getElementById("board")
 console.log(board);
+function creatBoard(index) {
+    let elem = document.createElement(`div`)
+    elem.setAttribute("id", `${index}`)
+    elem.classList.add(`cellClass`)
+    return elem
+
+}
+for (i in boardArr) {
+    for (j in boardArr[i]) {
+        board.append(creatBoard(String(i) + String(j)))
+    }
+
+}
