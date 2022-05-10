d1ed2d05e3bd90502887553f4962736262fb26af
Damri <damri@hp.com>
2022-05-10 19:18:07 +0300

    add event listner

--- XO.js
+++ XO.js
@@ -1,12 +1,18 @@
-let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]]
+let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]], player = 'x'
+
 const board = document.getElementById("board")
 console.log(board);
 function creatBoard(index) {
     let elem = document.createElement(`div`)
     elem.setAttribute("id", `${index}`)
     elem.classList.add(`cellClass`)
+    elem.addEventListener('click', clicked)
     return elem
 
+}
+
+function clicked(e) {
+    console.log(e.target);
 }
 for (i in boardArr) {
     for (j in boardArr[i]) {
