0394785cff7c7c0d978b4cdba38e506df8daf70e
ariel gold <ariel6185@gmail.com>
2022-05-23 21:49:08 +0300

    check win in rows & center the style

--- XO.js
+++ XO.js
@@ -1,49 +1,83 @@
-let boardArr = [["", "", ""], ["", "", ""], ["", "", ""]], player = 'x'
+let boardArr = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+],
+player = "x";
 
-const board = document.getElementById("board")
-console.log(board);
-function creatBoard(index) {
-    let elem = document.createElement(`div`)
-    elem.setAttribute("id", `${index}`)
-    elem.classList.add(`cellClass`)
-    elem.addEventListener('click', clicked)
-    return elem
-
-}
-function clicked(e) {
-    let location = getIDFromElemnt(e.target)
-    if (cellEmpty(location)) {
-        addPicToCell(e.target, location)
-        player = player == 'x' ? 'o' : 'x'
-        console.log(boardArr);
-        checkWin(boardArr, player)
-    }
-}
+const board = document.getElementById("board");
 for (i in boardArr) {
     for (j in boardArr[i]) {
-        board.append(creatBoard(String(i) + String(j)))
+        board.append(creatBoard(String(i) + String(j)));
     }
-
 }
+
+function creatBoard(index) {
+    let elem = document.createElement(`div`);
+    elem.setAttribute("id", `${index}`);
+    elem.classList.add(`cellClass`);
+    elem.addEventListener("click", clicked);
+    return elem;
+}
+
+function clicked(e) {
+    let location = getIDFromElemnt(e.target);
+    if (cellEmpty(location)) {
+        addPicToCell(e.target, location);
+        player = player == "x" ? "o" : "x";
+        checkRows();
+    }
+}
+
 function getIDFromElemnt(cell) {
-    return [cell.id[0], cell.id[1]]
+    return [cell.id[0], cell.id[1]];
 }
 function cellEmpty(location) {
-    return boardArr[location[0]][location[1]] == ""
+    return boardArr[location[0]][location[1]] == "";
 }
 function addPicToCell(cell, location) {
-    if (player == 'x') {
-        cell.classList.add("classX")
-        boardArr[location[0]][location[1]] = 'x'
+    if (player == "x") {
+        cell.classList.add("classX");
+        boardArr[location[0]][location[1]] = "x";
     } else {
-        cell.classList.add("classO")
-        boardArr[location[0]][location[1]] = 'o'
+        cell.classList.add("classO");
+        boardArr[location[0]][location[1]] = "o";
     }
 }
 
-function checkWin(boardArr, player) {
-    // debugger
-    if (boardArr[0].every((p) => { p == (player = player == 'x' ? 'o' : 'x') })) {
-        alert("youWin");
+function checkRows() {
+    for (i in boardArr) {
+        let check = boardArr[i][0];
+        for (j = 1; j < boardArr[i].length; j++) {
+            if (check == "") {
+                win = false;
+                break;
+            } else if (boardArr[i][j] != check) {
+                win = false;
+                break;
+            } else {
+                win = true;
+                winner = check;
+            }
+            check = boardArr[i][j];
+        }
+        if (win) {
+            console.log("the winner is " + winner);
+            break;
+        }
     }
 }
+
+// function checkWin(boardArr, player) {
+//     // debugger
+//     if (boardArr[0].every((p) => { p == (player = player == 'x' ? 'o' : 'x') })) {
+//         alert("youWin");
+//     }
+// }
+
+//     if(boardArr[0].every(p=>{console.log(p)})){
+//         alert("win")
+//     }
+// }
+
+// if (boardArr[0][0]==boardArr[0][1]==boardArr[0][2])
